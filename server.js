76df3c0a9320f95b003a85c6f90e848b29ef4dca
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-22 20:02:02 +0530

    Commit

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
