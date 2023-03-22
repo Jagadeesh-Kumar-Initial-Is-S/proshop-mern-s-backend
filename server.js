c37e2bcdaaf6f8247c910989d18874030ad95b91
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-22 21:04:14 +0530

    Commit

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
-const app = express()
-
-app.use(cors())
-
 dotenv.config()
 
 connectDB()
 
 
+const app = express()
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+
+app.use(cors())
 
 app.use(express.json())
 
